5d5bfc7a5627fa7b967a8ae28ba4cf2b56638b02
completelyabstract <completelyabstract@users.noreply.github.com>
2020-08-31 12:32:41 -0400

    test commit

---
+++ scripts/nightmode.js
@@ -0,0 +1,9 @@
+function nightmodeOn() {
+  document.body.style.color = "white";
+  document.body.style.backgroundColor = "#252530";
+}
+
+function nightmodeOff() {
+  document.body.style.color = "black";
+  document.body.style.backgroundColor = "#eee";
+}
